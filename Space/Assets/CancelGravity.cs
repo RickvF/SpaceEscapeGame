9301867af3a83802a8e30af44984ccef0e9c379b
Herman-Peter Leusink <Herman-Peter.Leusink@student.lamk.fi>
2018-11-14 12:06:57 +0200

    added crosshair and small changes

---
+++ Space/Assets/CancelGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CancelGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "FPSController")
+        {
+            Debug.Log(other.name);
+            other.GetComponent<FirstPersonController>().SetGravity(0);
+        }
+
+    }
+}
