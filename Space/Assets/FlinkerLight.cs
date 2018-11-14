d3f78abbf78393f3e49831015c7b09ec46412d57
Petrus Adrianus Johannes van Fessem <Petrus.vanFessem@student.lamk.fi>
2018-11-14 14:33:19 +0200

    add cell and light

---
+++ Space/Assets/FlinkerLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlinkerLight : MonoBehaviour {
+
+    public Light celLight;
+    private int duration = 100;
+    private float waitTime = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Flashing());
+		
+	}
+	
+	IEnumerator Flashing()
+    {
+        int i = 0;
+        for(i=0; i<duration; i++)
+        {
+            if(i%5 == 0)
+            {
+                waitTime -= 0.05f;
+            }
+            yield return new WaitForSeconds(waitTime);
+            celLight.enabled = !celLight.enabled;
+        }
+
+        celLight.enabled = false;
+    }
+}
---
+++ Space/Assets/autoOpenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoOpenDoor : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    public void OpenDoor()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
